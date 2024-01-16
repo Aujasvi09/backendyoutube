2b65bf2952533c9daf4079eebb4b8f95602472d9
unknown <aujas.chaudhry@gmail.com>
2024-01-17 03:00:08 +0530

    added asyncHandler error & apiResponse utils

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
-import dotenv from 'dotenv'
+// import dotenv from 'dotenv'
 import connectDB from './db/index.js'
+import express from 'express'
+import cors from "cors"
+import cookieParser from 'cookie-parser'
 
-dotenv.config({
-    path: "./env"
-})
 
+// Figure out .env config 
+// dotenv.config({
+//     path: "./env"
+// })
+
+const app = express()
+app.use(cors(
+    {
+        origin: "*",
+        credentials: true
+    }
+))
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.static("public"))
 
 connectDB()
+.then(() => {
+    app.listen(8000, () => {
+        console.log("App listening on port 8000")
+    })
+})
+.catch((err) => {
+    console.log("DB CONNECTION ERROR",err)
+})
